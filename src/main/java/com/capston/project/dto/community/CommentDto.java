3727fb6e3855189075f3916c2ed808beec4d6371
Subin Hong <subin5923@gmail.com>
2023-05-10 14:44:22 +0900

    localdatetime jsontype change

--- src/main/java/com/capston/project/dto/community/CommentDto.java
+++ src/main/java/com/capston/project/dto/community/CommentDto.java
@@ -1,9 +1,11 @@
 package com.capston.project.dto.community;
 
 import com.capston.project.entity.community.Comment;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +16,7 @@ public class CommentDto {
     private Long commentId;
     private String commentContent;
     private String commentWriter;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime commentCreatedAt;
 
     public static CommentDto toDto(Comment comment) {
