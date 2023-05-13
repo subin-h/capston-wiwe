f12c3f3a4f78169a2d752ece43d4e27f7a2fa6a4
Subin Hong <subin5923@gmail.com>
2023-05-14 01:13:33 +0900

    cottent fix2

--- src/main/java/com/capston/project/dto/community/CommentCreateDto.java
+++ src/main/java/com/capston/project/dto/community/CommentCreateDto.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentCreateDto {
-    @NotBlank
+
     private Long boardsId;
     @NotBlank
     private String commentContent;
