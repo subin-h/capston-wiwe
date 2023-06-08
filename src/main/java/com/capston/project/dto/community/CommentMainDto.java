40852b3447a3d589bca5b34a938718868928e470
Subin Hong <subin5923@gmail.com>
2023-06-08 12:14:23 +0900

    final fix

--- src/main/java/com/capston/project/dto/community/CommentMainDto.java
+++ src/main/java/com/capston/project/dto/community/CommentMainDto.java
@@ -2,10 +2,13 @@ package com.capston.project.dto.community;
 
 import com.capston.project.entity.community.Boards;
 import com.capston.project.entity.community.Comment;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -15,9 +18,17 @@ public class CommentMainDto {
     private Long commentId;
     private String commentContent;
     private Long boardsId;
+    private String commentWriter;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime commentCreatedAt;
 
     public CommentMainDto toDto(Comment comment ) {
-        return new CommentMainDto(comment.getCommentId(), comment.getCommentContent(), comment.getBoards().getBoardsId());
+        return new CommentMainDto(
+                comment.getCommentId(),
+                comment.getCommentContent(),
+                comment.getBoards().getBoardsId(),
+                comment.getUser().getNickname(),
+                comment.getCreatedAt());
     }
 
 }
