6d0012822b81105f8fdcd14e344ffb6e9f2126a8
Subin Hong <subin5923@gmail.com>
2023-06-08 11:58:40 +0900

    boards createdAt list add

--- src/main/java/com/capston/project/dto/community/BoardsMainDto.java
+++ src/main/java/com/capston/project/dto/community/BoardsMainDto.java
@@ -1,9 +1,12 @@
 package com.capston.project.dto.community;
 
 import com.capston.project.entity.community.Boards;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -14,7 +17,10 @@ public class BoardsMainDto {
     private String title;
     private String nickname;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime boardsDate;
+
     public BoardsMainDto toDto(Boards boards) {
-        return new BoardsMainDto(boards.getBoardsId(), boards.getBoardsTitle(),boards.getUser().getNickname());
+        return new BoardsMainDto(boards.getBoardsId(), boards.getBoardsTitle(),boards.getUser().getNickname(), boards.getCreatedAt());
     }
 }
